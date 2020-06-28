02b7f5d9c93353b6d15990d94a24c9e4673a9d72
ushapatturaj <ushadevipatturaj@gmail.com>
2020-06-29 00:40:41 +0200

    HyperSkill Codes - Who is Who and The Creator

---
+++ Problems/Who is who/task.py
@@ -0,0 +1,17 @@
+class Angel:
+    color = "white"
+    feature = "wings"
+    home = "Heaven"
+
+
+class Demon:
+    color = "red"
+    feature = "horns"
+    home = "Hell"
+
+
+my_angel = Angel()
+my_demon = Demon()
+
+print("{0}\n{1}\n{2}".format(my_angel.color, my_angel.feature, my_angel.home))
+print("{0}\n{1}\n{2}".format(my_demon.color, my_demon.feature, my_demon.home))
