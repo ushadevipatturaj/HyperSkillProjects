42fda43ed3fc749e93de9eadfece8033cb41a406
ushapatturaj <ushadevipatturaj@gmail.com>
2020-06-29 00:45:35 +0200

    HyperSkill Codes - RockBand

--- Problems/Let's rock/task.py
+++ Problems/Let's rock/task.py
@@ -1,8 +1,8 @@
 # start a RockBand here
 class RockBand:
     genre = "rock"
-    key_instruments =["electric guitar", "drums"]
+    key_instruments = ["electric guitar", "drums"]
     n_members = 4
 
 my_rockband = RockBand()
-print("{0}\n{1}\n{2}".format(my_rockband.genre,my_rockband.n_members,my_rockband.key_instruments))
+print("{0}\n{1}\n{2}".format(my_rockband.genre, my_rockband.n_members, my_rockband.key_instruments))
