d1997d79b778f6c7f5742e2874928eb426761650
ushapatturaj <ushadevipatturaj@gmail.com>
2020-06-30 16:28:22 +0200

    HyperSkill Codes - Class, Object, Methods - Piggy Bank

---
+++ Problems/Piggy bank/task.py
@@ -0,0 +1,11 @@
+class PiggyBank:
+    # create __init__ and add_money methods
+    def __init__(self, dollars, cents):
+        self.dollars = dollars
+        self.cents = cents
+    def add_money(self, deposit_dollars, deposit_cents):
+        self.cents += deposit_cents
+        self.dollars += deposit_dollars
+        if self.cents > 99:
+            self.dollars += self.cents // 100
+            self.cents = self.cents % 100
