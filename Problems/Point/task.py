71390fef0a2a1f6d7e0e65a9131237a60c5b34ca
ushapatturaj <ushadevipatturaj@gmail.com>
2020-06-30 12:52:56 +0200

    HyperSkill Codes - Class, Object, Methods - Distance between 2 points

---
+++ Problems/Point/task.py
@@ -0,0 +1,9 @@
+import math
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dist(self, point1):
+        d = math.sqrt(((self.x - point1.x) ** 2) + ((self.y - point1.y) ** 2))
+        return d
