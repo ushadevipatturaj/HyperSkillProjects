0a9599b51f17ff7a11bb6723970ad9b917665320
ushapatturaj <ushadevipatturaj@gmail.com>
2020-06-30 12:34:59 +0200

    HyperSkill Codes - Class, Object, Methods - Ship

---
+++ Problems/Keep on sailing/task.py
@@ -0,0 +1,16 @@
+# our class Ship
+class Ship:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+        self.cargo = 0
+
+    # the old sail method that you need to rewrite
+    def sail(self, destination):
+        print("The {name_of_the_ship} has sailed for {city}!".format(name_of_the_ship=self.name,
+                                                                        city=destination))
+
+
+black_pearl = Ship("Black Pearl", 800)
+city_name = input()
+black_pearl.sail(city_name)
