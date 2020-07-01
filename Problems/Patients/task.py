c53fda95a1dd871eb5f2b56474366968ea608bb2
ushapatturaj <ushadevipatturaj@gmail.com>
2020-07-01 17:11:25 +0200

    HyperSkill Codes - Class, Object, Methods - __repr__ and __str__

---
+++ Problems/Patients/task.py
@@ -0,0 +1,11 @@
+class Patient:
+    def __init__(self, name, last_name, age):
+        self.name = name
+        self.last_name = last_name
+        self.age = age
+
+    # create methods here
+    def __repr__(self):
+        return "Object of the class Patient. name: {name}, last_name: {last_name}, age: {age}".format(name=self.name, last_name=self.last_name, age=self.age)
+    def __str__(self):
+        return "{name} {last_name}. {age}".format(name=self.name, last_name=self.last_name, age=self.age)
